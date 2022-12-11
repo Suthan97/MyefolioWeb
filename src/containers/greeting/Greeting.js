f325bd129743a4e1297d66d1769c120cc9b9f4cd
Suthan Amalathas <60068030+Suthan97@users.noreply.github.com>
2022-12-12 01:46:33 +0530

    Files push locally

---
+++ src/containers/greeting/Greeting.js
@@ -0,0 +1,77 @@
+import React from "react";
+import "./Greeting.css";
+// import SocialMedia from "../../components/socialMedia/SocialMedia";
+import Button from "../../components/button/Button";
+import { greeting } from "../../portfolio";
+import { Fade } from "react-reveal";
+import BannerImg from "./BannerImg";
+import Typewriter from "typewriter-effect";
+import CompetitiveSites from "../../components/competitiveSites/CompetitiveSites";
+import { competitiveSites } from "../../portfolio";
+export default function Greeting(props) {
+  const theme = props.theme;
+  return (
+    <Fade bottom duration={2000} distance="40px">
+      <div className="greet-main" id="greeting">
+        <div className="greeting-main">
+          <div className="greeting-text-div">
+            <div>
+              <h1 className="greeting-text" style={{ color: theme.orange }}>
+                {greeting.title}
+              </h1>
+              <h1 className="greeting-sub" style={{ color: theme.grayText }}>
+                I'm <mark data-entity="person">{greeting.sub}</mark>
+              </h1>
+              <h1 className="greeting-typewriter" style={{color: theme.blue}}>
+                <Typewriter
+                  onInit={(typewriter) => {
+                    typewriter
+                      .typeString("Data Science")
+                      .pauseFor(200)
+                      .deleteAll()
+                      .typeString("Web Development")
+                      .pauseFor(200)
+                      .deleteAll()
+                      //.typeString("UI/UX Designer")
+                      //.pauseFor(200)
+                      //.deleteAll()
+                      //.typeString("IT Engineer")
+                      //.pauseFor(200)
+                      .start();
+                  }}
+                  options={{
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </h1>
+              <span
+                className="greeting-text-span subTitle"
+                style={{ color: theme.text }}>
+                <div className="entities">
+                  A <mark data-entity="noun">Data Science enthusiast</mark>{" "}
+                  who thrives to{" "}
+                  <mark data-entity="verb">prediction results</mark> using
+                  ML-based solutions along with{" "}
+                  <mark data-entity="skill">Web Development</mark>
+                </div>
+              </span>
+              <CompetitiveSites logos={competitiveSites.competitiveSites} />
+              <div className="button-greeting-div">
+                <Button text="Contact me" href="/contact" />
+                <Button
+                  text="See my resume"
+                  newTab={true}
+                  href={greeting.resumeLink}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="greeting-image-div">
+            <BannerImg />
+          </div>
+        </div>
+      </div>
+    </Fade>
+  );
+}
